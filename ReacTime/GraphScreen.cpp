ef331cb45791b4f477728b7b445f78b2c1dd226d
GG <ballisticc@gmx.de>
2026-01-14 18:39:50 +0100

    Added vertex base functionality in GraphScreen
    GraphScreen now holds a vertexVector for storing test data.
    GraphScreen now supports drawing said vertexVector and writing (dynamically) to it.
    Vertices are represented by sprites with the added "Point.png" texture.
    GraphScreen writeVertex func accounts for the right coordinates by mapping the input pos to the visual graph.

--- ReacTime/GraphScreen.cpp
+++ ReacTime/GraphScreen.cpp
@@ -2,12 +2,35 @@
 
 GraphScreen::GraphScreen() : graphBackground(graphTexture){
 	if (!graphTexture.loadFromFile("../assets/textures/GraphScreen.png"))
-		printf("Graph Background Texture could not be loaded from file! ");
+		printf("Graph Background Texture could not be loaded from file! \n");
+	if (!vertexTexture.loadFromFile("../assets/textures/Point.png"))
+		printf("Vertex Texture could not be loaded from file! \n");
 	graphBackground.setTexture(graphTexture);
 	graphBackground.setTextureRect(sf::IntRect({ 0,0 }, { 1200,675 }));
+
+	vertexArray.reserve(100);
+	//writeVertex({ 0.f,0.f });
 }
 
 void GraphScreen::draw(sf::RenderWindow& window) {
 	//printf("Graph Screen is being drawn!\n");
 	window.draw(graphBackground);
+
+	for (const auto& vertex : vertexVector) {
+		window.draw(vertex);
+	}
+}
+
+void GraphScreen::writeVertex(sf::Vector2f pos) {
+	if (vertexVector.size() >= 100)
+		return;
+
+	sf::Sprite sprite(vertexTexture);
+	
+	sprite.setTextureRect(sf::IntRect({0,0}, {5, 5}));
+	sprite.setScale({ 3.f,3.f });
+	sprite.setOrigin(sf::Vector2f{2.5f, 2.5f});
+	sprite.setPosition({80.f + pos.x, WindowHeight - 80.f - pos.y});
+
+	vertexVector.emplace_back(sprite);
 }
--- ReacTime/GraphScreen.h
+++ ReacTime/GraphScreen.h
@@ -7,11 +7,13 @@ private:
 	sf::Sprite graphBackground;
 	sf::Texture graphTexture;
 
+	sf::Texture vertexTexture;
+	std::vector<sf::Sprite> vertexVector;
 
 public:
 	GraphScreen();
 
 	void draw(sf::RenderWindow& window) override;
 
-
+	void writeVertex(sf::Vector2f pos);
 };
